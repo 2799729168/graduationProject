b41528302ee26f81d550085b6469e0a052e0358c
pb28378 <bing.peng01@hand-china.com>
2020-10-22 15:04:43 +0800

    项目建立

---
+++ graproject/src/test/java/com/graproject/GraprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
