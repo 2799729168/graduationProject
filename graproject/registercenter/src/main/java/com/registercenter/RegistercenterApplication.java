dcbba26ef45c2fa254ad6e56749d0c488ca346d8
pb28378 <bing.peng01@hand-china.com>
2020-10-22 16:19:53 +0800

    项目建立

--- graproject/registercenter/src/main/java/com/registercenter/RegistercenterApplication.java
+++ graproject/registercenter/src/main/java/com/registercenter/RegistercenterApplication.java
@@ -2,8 +2,13 @@ package com.registercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author pb
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class RegistercenterApplication {
 
     public static void main(String[] args) {
