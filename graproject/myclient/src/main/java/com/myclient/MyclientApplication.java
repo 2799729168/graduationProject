e773c28bcb01066ab496dfc2e5fe4a69d6aed146
pb28378 <bing.peng01@hand-china.com>
2020-10-22 16:20:11 +0800

    项目建立

---
+++ graproject/myclient/src/main/java/com/myclient/MyclientApplication.java
@@ -0,0 +1,16 @@
+package com.myclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MyclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyclientApplication.class, args);
+    }
+
+}
