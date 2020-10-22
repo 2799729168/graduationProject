bb3ddada27e4fda5743d815764442df7c83892b5
pb28378 <bing.peng01@hand-china.com>
2020-10-22 15:34:49 +0800

    项目建立

--- graproject/registercenter/src/main/java/com/registercenter/RegistercenterApplication.java
+++ graproject/registercenter/src/main/java/com/registercenter/RegistercenterApplication.java
@@ -1,16 +1,13 @@
-package com.example.demo;
+package com.registercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author pb
- */
 @SpringBootApplication
-public class DemoApplication {
+public class RegistercenterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(RegistercenterApplication.class, args);
     }
 
 }
