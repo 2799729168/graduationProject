022b9842887a6c0ec879dcdb20d6ce0d57c4f10e
pb28378 <bing.peng01@hand-china.com>
2020-10-23 13:19:29 +0800

    项目建立

---
+++ graproject/myclient2/src/main/java/com/myclient2/Myclient2Application.java
@@ -0,0 +1,18 @@
+package com.myclient2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author pb
+ */
+@SpringBootApplication
+@EnableSwagger2
+public class Myclient2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Myclient2Application.class, args);
+    }
+
+}
---
+++ graproject/myclient2/src/test/java/com/myclient2/Myclient2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myclient2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myclient2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
