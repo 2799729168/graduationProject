ecc44a4049fd95f19fdd19fa839bf4e83a93df83
pb28378 <bing.peng01@hand-china.com>
2020-11-04 08:07:45 +0800

    项目建立

--- graproject/myclient/src/main/java/com/myclient/MyclientApplication.java
+++ graproject/myclient/src/main/java/com/myclient/MyclientApplication.java
@@ -3,6 +3,8 @@ package com.myclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -11,6 +13,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableSwagger2
+//扫描到通用包
+@ComponentScans(value = {@ComponentScan(value = "com.baseutils")})
 public class MyclientApplication {
 
     public static void main(String[] args) {
